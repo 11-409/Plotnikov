af073c424b392fc1351b0260b1e81286e8631e66
Keensu <yabizunda@gmail.com>
2025-05-07 16:53:54 +0300

    Add files via upload

---
+++ Tree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Node
+    {
+        public int data; //данные
+        public Node Left;
+        public Node Right;
+
+        public Node(int x)
+        {
+            data = x;
+            Left = null;
+            Right = null;
+        }
+    }
+}
