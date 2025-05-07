8ba095c5f1623044d601016c9864e17c350f8fb5
Keensu <yabizunda@gmail.com>
2025-05-07 17:40:02 +0300

    Add files via upload

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tree;
+
+Traversal travel = new Traversal();
+
+Node root = new Node(1);
+Node node2 = new Node(2);
+Node node3 = new Node(3);
+Node node4 = new Node(4);
+Node node5 = new Node(5);
+
+root.Left = node2;
+root.Right = node3;
+node2.Left = node4;
+node2.Right = node5;
+
+Console.WriteLine(travel.Depth(root));
+Console.WriteLine(travel.ElementsSum(root));
+
+
+
+
+
