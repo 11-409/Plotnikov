12b489e33e83ada8cda84fdfa9252cb430d80aca
Keensu <yabizunda@gmail.com>
2025-05-07 16:19:58 +0300

    Add files via upload

---
+++ StackReverseString/StackReverseString/Stack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackReverseString
+{
+    class StackReverse
+    {
+        public void ReverseString()
+        {
+            Stack <int> stack = new Stack<int>();
+            string inputString = Console.ReadLine();
+            foreach (char letter in inputString)
+            {
+                stack.Push(letter);
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop);
+            }
+        }
+    }
+}
